296a11674db86d51595a1a0b99a5fae83f34eccc
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-03-20 16:08:42 -0300

    -Implement aligned allocations

--- example/main.cpp
+++ example/main.cpp
@@ -1,29 +1,111 @@
 #include <iostream>
+#include <chrono>
 #include <stack_allocator.h>
+#include <vector>
 
-struct MyClass {
+struct MyStruct {
+    MyStruct(){
+        std::cout << "Constructed MyStruct!" << std::endl;
+    }
+
+    ~MyStruct(){
+        std::cout << "Destructed MyStruct!" << std::endl;
+    }
+
+    int a = 0;
+    int b = 0;
+    double c = 0;
+};
+
+
+class MyClass {
+public:
     MyClass(){
-        std::cout << "Constructed!" << std::endl;
+        std::cout << "Constructed MyClass!" << std::endl;
     }
 
     ~MyClass(){
-        std::cout << "Destructed!" << std::endl;
+        std::cout << "Destructed MyClass!" << std::endl;
     }
 
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 0;
 };
+
+TypedStackAllocator typedStackAllocator(1024 * 1024);
+
+template<typename TAlloc, typename TDelete>
+size_t test(const std::vector<size_t>& sizes, int iterations, TAlloc alloc, TDelete del){
+    size_t accumulatedDuration = 0;
+    float totalSum = 0;
+    for (int i = 0; i < iterations; i++) {
+        int qty = sizes[i];
+        auto start = std::chrono::steady_clock::now();
+        auto myFloats = alloc(qty);
+        auto end = std::chrono::steady_clock::now();
+        accumulatedDuration += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        float sum = 0;
+        for (int j = 0; j < qty; j++){
+            myFloats[j].a = sizes[i];
+            sum += myFloats[j].a;
+        }
+        totalSum += sum;
+        start = std::chrono::steady_clock::now();
+        del(myFloats);
+        end = std::chrono::steady_clock::now();
+        accumulatedDuration += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    }
+    return accumulatedDuration / iterations;
+}
 
 
 int main() {
 
-    TypedStackAllocator typedStackAllocator(1024);
+//    srand(time(NULL));
 
-    auto myClasses = typedStackAllocator.construct<MyClass>(3);
+//    const int iterations = 1000;
+//    const int allocationSize = 10000;
+//    size_t stackAccumulatedDuration = 0;
+//    size_t newAccumulatedDuration = 0;
+//
+//    for (int i = 0; i < 1000; i++) {
+//        std::vector<size_t> allocationSizes(iterations);
+//        for (auto &size : allocationSizes) {
+//            size = (rand() % allocationSize) + 1;
+//        }
+//
+//        stackAccumulatedDuration += test(allocationSizes, iterations, [](size_t qty) -> MyClass * {
+//            return typedStackAllocator.construct<MyClass>(qty);
+//        }, [](MyClass *MyClasss) {
+//            typedStackAllocator.pop();
+//        });
+//
+//        newAccumulatedDuration += test(allocationSizes, iterations, [](size_t qty) -> MyClass * {
+//            return new MyClass[qty];
+//        }, [](MyClass *MyClasss) {
+//            delete[] MyClasss;
+//        });
+//    }
+//
+//    std::cout << "Stack duration: " << stackAccumulatedDuration / 1000 << std::endl;
+//    std::cout << "New duration: " << newAccumulatedDuration / 1000 << std::endl;
 
-    typedStackAllocator.pop();
+//    TypedStackAllocator stackAllocator(1024);
 
+    TypedStackAllocator stack(1024 * 1024);
+    {
+        auto scope =  stack.scope();
+        auto myArr1 = scope.construct<MyClass>(10);
+        auto myArr2 = scope.construct<MyClass>(10);
+        auto myArr3 = scope.construct<MyClass>(14);
+        auto myArr4 = scope.construct<MyClass>(12);
+        auto myArr5 = scope.construct<MyClass>(10);
+        auto myArr6 = scope.construct<MyClass>(10);
+    }
+
+    assert(stack.marker() == 0);
 
     return 0;
 }
--- stack_allocator.h
+++ stack_allocator.h
@@ -15,9 +15,16 @@ public:
 
 
     void* alloc(size_t size) {
+        assert(m_marker + size < m_size && "Stack overflow");
         uintptr_t address = reinterpret_cast<uintptr_t>(m_block) + m_marker;
         m_marker += size;
         return reinterpret_cast<void*>(address);
+    }
+
+    void* alloc_aligned(size_t size, size_t alignment){
+        assert((alignment & (alignment - 1)) == 0 && "Requested alignment was not a power of 2");
+        auto rawAddress = reinterpret_cast<uintptr_t>(alloc(size + alignment));
+        return reinterpret_cast<void*>(next_aligned_address(rawAddress, alignment));
     }
 
     size_t marker() const { return m_marker; }
@@ -25,6 +32,14 @@ public:
     void free_to_marker(size_t marker){
         assert(marker >= 0);
         m_marker = marker;
+    }
+protected:
+
+    inline static uintptr_t next_aligned_address(uintptr_t address, size_t alignment){
+        size_t mask = alignment - 1;
+        uintptr_t misalignment = address & mask;
+        ptrdiff_t adjustment = alignment - misalignment;
+        return address + adjustment;
     }
 
 protected:
@@ -39,38 +54,69 @@ private:
     typedef void (*DestructorFunc)(void*, size_t);
 public:
 
+    class Scope {
+    public:
+        Scope(TypedStackAllocator& allocator) : m_allocator(allocator) {}
+
+        ~Scope() {
+            for (int i = 0; i < m_allocationCount; ++i) {
+                m_allocator.pop();
+            }
+        }
+
+        template<typename T, typename ...Args>
+        T* construct(size_t quantity = 1, Args&& ...args) {
+            m_allocationCount++;
+            return m_allocator.construct<T, Args...>(quantity, std::forward<Args>(args)...);
+        }
+
+    private:
+        TypedStackAllocator& m_allocator;
+        size_t m_allocationCount = 0;
+    };
+
     explicit TypedStackAllocator(size_t size) : StackAllocator(size) {}
     ~TypedStackAllocator() override = default;
 
+    Scope scope() { return Scope(*this); }
+
     template<typename T, typename ...Args>
-    T* construct(size_t quantity = 1, Args&& ...args) {
+    T *construct(size_t quantity = 1, Args &&...args) {
         assert(quantity >= 1);
 
         size_t currentMarker = marker();
-        auto destructorAddress = (DestructorFunc*)alloc(sizeof(DestructorFunc));
+        auto destructorAddress = (DestructorFunc *) alloc(sizeof(DestructorFunc));
         *destructorAddress = destruct<T>;
 
-        auto quantityAddress = (size_t*)alloc(sizeof(size_t));
+        auto quantityAddress = (size_t *) alloc(sizeof(size_t));
         *quantityAddress = quantity;
 
-        T* obj = (T*)alloc(sizeof(T) * quantity);
-        T* objIterator = obj;
-        for (int i = 0; i < quantity; i++){
+        auto alignmentAddress = (size_t*)alloc(sizeof(size_t));
+        *alignmentAddress = alignof(T);
+
+        T *obj = (T*)alloc_aligned(sizeof(T) * quantity, alignof(T));
+        T *objIterator = obj;
+        for (int i = 0; i < quantity; i++) {
             ::new(objIterator) T(std::forward(args)...);
             objIterator++;
         }
-        auto lastMarkerAddress = (size_t*)alloc(sizeof(size_t));
+        auto lastMarkerAddress = (size_t *) alloc(sizeof(size_t));
         *lastMarkerAddress = currentMarker;
         return obj;
     }
 
-    void pop(){
-        auto lastMarkerAddress = reinterpret_cast<size_t*>(reinterpret_cast<uintptr_t>(m_block) + m_marker - sizeof(size_t));
-        auto destructorAddress = reinterpret_cast<DestructorFunc*>(reinterpret_cast<uintptr_t>(m_block) + *lastMarkerAddress);
-        auto quantityAddress = reinterpret_cast<size_t*>(reinterpret_cast<uintptr_t>(destructorAddress) + sizeof(DestructorFunc*));
-        auto objAddress = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(quantityAddress) + sizeof(size_t));
+    void pop() {
+        auto lastMarkerAddress = reinterpret_cast<size_t *>(reinterpret_cast<uintptr_t>(m_block) + m_marker - sizeof(size_t));
+        auto destructorAddress = reinterpret_cast<DestructorFunc *>(reinterpret_cast<uintptr_t>(m_block) + *lastMarkerAddress);
+        auto quantityAddress = reinterpret_cast<size_t *>(reinterpret_cast<uintptr_t>(destructorAddress) + sizeof(DestructorFunc*));
+        auto alignmentAddress = reinterpret_cast<size_t *>(reinterpret_cast<uintptr_t>(quantityAddress) + sizeof(size_t));
+        auto objAddress = reinterpret_cast<void*>(next_aligned_address(reinterpret_cast<uintptr_t>(alignmentAddress) + sizeof(size_t), *alignmentAddress));
         (*destructorAddress)(objAddress, *quantityAddress);
         free_to_marker(*lastMarkerAddress);
+    }
+
+    size_t marker() const {
+        return m_marker;
     }
 
 private:
